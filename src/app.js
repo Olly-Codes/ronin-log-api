7ab04cb56b3022a0841953124c0668a29eecfb1c
Olly <ollycodesdev@gmail.com>
2026-07-04 15:32:38 +0200

    feat: add and import index router

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import express from "express";
+
+import indexRouter from "./routes/indexRouter.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", indexRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (err) => {
+    if (err) {
+        throw err;
+    };
+    console.log(`Listening on port ${PORT}`);
+});
