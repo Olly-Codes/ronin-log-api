0b3687e82a16781f674c04aad1a62eff495e1fc9
Olly <ollycodesdev@gmail.com>
2026-07-04 16:12:41 +0200

    feat: add and import reviews router

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 
 import indexRouter from "./routes/indexRouter.js";
+import reviewsRouter from "./routes/reviewsRouter.js";
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", indexRouter);
+app.use("/reviews", reviewsRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, (err) => {
