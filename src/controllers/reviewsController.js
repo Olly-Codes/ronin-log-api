e25475995bac9b4dfdd40acfe5b4986d3042712d
Olly <ollycodesdev@gmail.com>
2026-07-05 13:05:28 +0200

    feat: add review details controller logic to fetch a review with comments

--- src/controllers/reviewsController.js
+++ src/controllers/reviewsController.js
@@ -13,6 +13,25 @@ const getAllPublishedReviews = async (req, res, next) => {
     }
 };
 
+const getPublishedReview = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const review = await db.getPublishedReviewDetails(id);
+
+        if (!review) return res.status(404).json({ error: "Review not found" });
+
+        const comments = await db.getPublishedReviewComments(id);
+
+        res.status(200).json({
+            review,
+            comments,
+        });
+    } catch (err) {
+        next(arr);
+    }
+};
+
 export default {
-    getAllPublishedReviews
+    getAllPublishedReviews,
+    getPublishedReview
 };
--- src/routes/reviewsRouter.js
+++ src/routes/reviewsRouter.js
@@ -4,5 +4,6 @@ import reviewsController from "../controllers/reviewsController.js";
 const reviewsRouter = Router();
 
 reviewsRouter.get("/", reviewsController.getAllPublishedReviews);
+reviewsRouter.get("/details/:id", reviewsController.getPublishedReview);
 
 export default reviewsRouter;
