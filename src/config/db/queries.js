04729414357d01e6ec944b5291294bef677877bb
Olly <ollycodesdev@gmail.com>
2026-07-06 15:45:23 +0200

    feat: add query to fetch a reviews details for admins

--- src/config/db/queries.js
+++ src/config/db/queries.js
@@ -33,6 +33,22 @@ const getAllPublishedReviews = async () => {
     return rows;
 }
 
+const getReviewDetails = async (reviewId) => {
+    const { rows } = await pool.query(`
+        SELECT r.review_id, r.title, r.body, r.score, r.cover_image_url,
+        r.published, r.created_at, d.name AS demographic,
+        mt.name AS media_type, json_agg(g.name) AS genres
+        FROM reviews r
+        LEFT JOIN demographics d ON r.demographic_id = d.demographic_id
+        LEFT JOIN media_type mt ON r.media_type_id = mt.media_type_id
+        LEFT JOIN review_genres rg ON r.review_id = rg.review_id
+        LEFT JOIN genres g ON rg.genre_id = g.genre_id
+        WHERE r.review_id = $1
+        GROUP BY r.review_id, d.name, mt.name;`, [reviewId]
+    );
+    return rows[0];
+};
+
 const getPublishedReviewDetails = async (reviewId) => {
     const { rows } = await pool.query(`
         SELECT r.review_id, r.title, r.body, r.score, r.cover_image_url,
