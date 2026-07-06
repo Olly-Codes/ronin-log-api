ce02b681af0a170c07b4c5f92bc92de834905230
Olly <ollycodesdev@gmail.com>
2026-07-06 15:43:50 +0200

    feat: add query to fetch all reviews for admin roles

--- src/config/db/queries.js
+++ src/config/db/queries.js
@@ -1,5 +1,21 @@
 import pool from "./pool.js";
 
+const getAllReviews = async () => {
+    const { rows } = await pool.query(`
+        SELECT r.review_id, r.title, r.score, r.cover_image_url,
+        r.published, r.created_at, d.name AS demographic,
+        mt.name AS media_type, json_agg(g.name) AS genres
+        FROM reviews r
+        LEFT JOIN demographics d ON r.demographic_id = d.demographic_id
+        LEFT JOIN media_type mt ON r.media_type_id = mt.media_type_id
+        LEFT JOIN review_genres rg ON r.review_id = rg.review_id
+        LEFT JOIN genres g ON rg.genre_id = g.genre_id
+        GROUP BY r.review_id, d.name, mt.name
+        ORDER BY r.created_at DESC;
+    `);
+    return rows;    
+}
+
 const getAllPublishedReviews = async () => {
     const { rows } = await pool.query(`
         SELECT r.review_id, r.title, r.score, r.cover_image_url,
@@ -143,6 +159,7 @@ const patchExistingReview = async (
 };
 
 export default {
+    getAllReviews,
     getAllPublishedReviews,
     getPublishedReviewDetails,
     getPublishedReviewComments,
