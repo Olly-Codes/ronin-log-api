58fe9ceac53e5112fef608efcd15b90c7d58da1d
Olly <ollycodesdev@gmail.com>
2026-07-07 12:19:09 +0200

    feat: add and import media types  router

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import reviewsRouter from "./routes/reviewsRouter.js";
 import commentsRouter from "./routes/commentsRouter.js";
 import genresRouter from "./routes/genresRouter.js";
 import demographicsRouter from "./routes/demographicsRouter.js";
+import mediaTypesRouter from "./routes/mediaTypesRouter.js";
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.use("/reviews", reviewsRouter);
 app.use("/comments", commentsRouter);
 app.use("/genres", genresRouter);
 app.use("/demographics", demographicsRouter);
+app.use("/media-types", mediaTypesRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, (err) => {
