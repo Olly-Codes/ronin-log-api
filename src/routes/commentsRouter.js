c39de91f4e0648bceab8b6db24082210b5d51184
Olly <ollycodesdev@gmail.com>
2026-07-12 23:59:47 +0200

    feat: add auth and admin checks to routes

--- src/routes/commentsRouter.js
+++ src/routes/commentsRouter.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import commentsController from "../controllers/commentsController.js";
+import passport from "passport";
+import { requireAdmin } from "../config/middleware/auth.js";
 
 const commentsRouter = Router();
 
-commentsRouter.delete("/:id", commentsController.deleteComment);
+commentsRouter.delete("/:id", passport.authenticate("jwt", { session: false }), requireAdmin, commentsController.deleteComment);
 
 export default commentsRouter;
--- src/routes/reviewsRouter.js
+++ src/routes/reviewsRouter.js
@@ -1,17 +1,19 @@
 import { Router } from "express";
 import reviewsController from "../controllers/reviewsController.js";
 import commentsController from "../controllers/commentsController.js";
+import passport from "passport";
+import { requireAdmin } from "../config/middleware/auth.js";
 
 const reviewsRouter = Router();
 
 reviewsRouter.get("/", reviewsController.getAllPublishedReviews);
 reviewsRouter.get("/:id", reviewsController.getPublishedReview);
 
-reviewsRouter.post("/", reviewsController.postCreateReview);
-reviewsRouter.post("/:id/comments", commentsController.postCreateComment);
+reviewsRouter.post("/", passport.authenticate("jwt", { session: false }), requireAdmin, reviewsController.postCreateReview);
+reviewsRouter.post("/:id/comments", passport.authenticate("jwt", { session: false }), commentsController.postCreateComment);
 
-reviewsRouter.patch("/:id", reviewsController.patchReview);
+reviewsRouter.patch("/:id", passport.authenticate("jwt", { session: false }), requireAdmin, reviewsController.patchReview);
 
-reviewsRouter.delete("/:id", reviewsController.deleteReview);
+reviewsRouter.delete("/:id", passport.authenticate("jwt", { session: false }), requireAdmin, reviewsController.deleteReview);
 
 export default reviewsRouter;
