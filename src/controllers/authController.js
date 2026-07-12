241d4327e77461e999f3644f1f90befaf9b34d4e
Olly <ollycodesdev@gmail.com>
2026-07-12 22:59:05 +0200

    feat: add auth controller logic to register new users

---
+++ src/controllers/authController.js
@@ -0,0 +1,49 @@
+import bcrypt from "bcrypt";
+import db from "../config/db/queries.js";
+import JWT from "../utils/issueJwt.js";
+
+const postRegister = async (req, res, next) => {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+        return res.status(400).json(
+            {
+                error: "Missing required fields"
+            }
+        )
+    }
+
+    try {
+        const user = await db.getUserByEmail(email);
+
+        if (user) {
+            return res.status(409).json(
+                {
+                    error: "Account already exists"
+                }
+            );
+        }
+
+        const hashedPass = await bcrypt.hash(password, 10);
+        const newUser = await db.postCreateUser(username, email, hashedPass);
+
+        if (newUser) {
+            const jwt = JWT.issueJWT(newUser);
+            res.status(201).json(
+            {
+                id: newUser.user_id,
+                username: newUser.username,
+                email: newUser.email,
+                created_at: newUser.created_at,
+                token: jwt.token
+            }
+        )   
+        }
+    } catch (err) {
+        next(err);
+    }
+}
+
+export default {
+    postRegister
+}
