9645dd8d9102fc568c952be52687c765e34c034e
Olly <ollycodesdev@gmail.com>
2026-07-16 02:13:40 +0200

    feat: add get comments route

--- src/routes/commentsRouter.js
+++ src/routes/commentsRouter.js
@@ -5,6 +5,7 @@ import { requireAdmin } from "../config/middleware/auth.js";
 
 const commentsRouter = Router();
 
+commentsRouter.get("/", passport.authenticate("jwt", { session: false }), requireAdmin, commentsController.getComments);
 commentsRouter.delete("/:id", passport.authenticate("jwt", { session: false }), requireAdmin, commentsController.deleteComment);
 
 export default commentsRouter;
