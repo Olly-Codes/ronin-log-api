2fbd8b76aa79805f664aee7f93aa85a1c2ada67c
Olly <ollycodesdev@gmail.com>
2026-07-31 16:41:48 +0200

    server errors not showing

--- src/app.js
+++ src/app.js
@@ -42,9 +42,7 @@ app.use((err, req, res, next) => {
     const errorMessage = statusCode === 500 ?
     "Something went wrong on our end!" : err.message;
 
-    if (process.env.NODE_ENV !== "production") {
         console.error(err.stack);
-    }
 
     res.status(statusCode).json(
         {
