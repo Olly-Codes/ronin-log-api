296ec824cb38d1589fa083c6e1eca2205969f7d2
Olly <ollycodesdev@gmail.com>
2026-07-31 16:21:49 +0200

    fix: add ssl config for supabase connection

--- src/config/db/pool.js
+++ src/config/db/pool.js
@@ -4,5 +4,6 @@ import { Pool } from "pg";
 const isProduction = process.env.NODE_ENV === "production";
 
 export default new Pool({
-    connectionString: isProduction? process.env.DB_PROD : process.env.DB_DEV
+    connectionString: isProduction? process.env.DB_PROD : process.env.DB_DEV,
+    ssl: isProduction ? { rejectUnauthorized: false } : false 
 });
