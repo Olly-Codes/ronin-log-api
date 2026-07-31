ac3d17d735fa5d6c9f4f2e7d023085f19c7473ff
Olly <ollycodesdev@gmail.com>
2026-07-31 17:41:53 +0200

    fix: cors refusal

--- src/app.js
+++ src/app.js
@@ -13,12 +13,22 @@ import authRouter from "./routes/authRouter.js";
 import adminRouter from "./routes/adminRouter.js";
 
 const app = express();
+const allowedOrigins = [
+    "http:localhost:5173",
+    process.env.CLIENT_URL
+];
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 app.use(cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    }
 }));
 
 app.use("/", indexRouter);
